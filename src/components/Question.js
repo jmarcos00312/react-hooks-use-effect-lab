5cadcd0b1a891c37859854033beaa1cd99a817ef
Jeremiah <jeremiahmarcos27@outlook.com>
2021-11-08 09:42:29 -0600

    completed assingment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,15 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    //if state is greater than or equal than zero
+    if (timeRemaining >= 0) {
+      //count down the timer remaining and set it to timerZero
+      const timerZero = setTimeout(() => {
+        //count down
+        setTimeRemaining(timeRemaining => timeRemaining - 1)
+        //every 1 second
+      }, 1000)
+      //cleanup anonymous function
+      return () => clearTimeout(timerZero)
+
+    } else {
+      //call the onAnswered prop and setting the value to false
+      onAnswered(false)
+    }
+  })
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
-
   const { id, prompt, answers, correctIndex } = question;
 
   return (
